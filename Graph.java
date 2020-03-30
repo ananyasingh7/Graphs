25dd98df8813b984e2829fc81f4d2e0c69fa8107
Ananya Singh <ananyasingh@Ananyas-Air.home>
2020-03-30 19:17:39 -0400

    basic layout should be done

--- Graph.java
+++ Graph.java
@@ -8,20 +8,23 @@ class Graph{
         listOfNodes = new ArrayList<Node>();
     }
 
-    void addNode(final String nodeVal){ //adds a new node to the Graph
-
+    void addNode(final int nodeVal){ //adds a new node to the Graph
+        Node newNode = new Node(nodeVal);
+        listOfNodes.add(newNode);
     }
 
-    void addUndirectedEdge(final Node first, final Node second){ //Adds an undirected edge between first and second
-
+    void addUndirectedEdge(final Node first, final Node second){ //adds an undirected edge between first and second
+        first.neighbors.add(second);
+        second.neighbors.add(first);
     }
 
-    void removeUndirectedEdge(final Node first, final Node second){ //removes an edge between first and second
-
+    void removeUndirectedEdge(final Node first, final Node second){ //removes an undirected edge between first and second
+        first.neighbors.remove(second);
+        second.neighbors.remove(first);
     }
 
     HashSet<Node> getAllNodes(){ //returns all the Nodes in the graph
-        HashSet<Node> nodes = new HashSet<Node>();
+        HashSet<Node> nodes = new HashSet<Node>(listOfNodes);
         return nodes;
     }
 
