e29875d6ce3f26bae16255e9931554addcb8ae9d
Ananya Singh <ananyasingh@Ananyas-Air.home>
2020-03-31 02:41:38 -0400

    finished 3b

--- Main.java
+++ Main.java
@@ -1,71 +1,46 @@
-import java.util.HashSet;
-
+import java.util.Random;
 
 class Main{
 
+    static Graph createRandomUnweightedGraphIter(int n){ //creates n random nodes with randomly assigned unweighted,  bidirectional edges
+        Graph graph = new Graph();
+
+        for(int i = 0; i<n; i++){
+            graph.addNode(i);
+        }
+        Random rand = new Random();
+        int counter = rand.nextInt(n);
+
+        while(counter > 0){
+            int randomNode1 = rand.nextInt(n);
+            int randomNode2 = rand.nextInt(n);
+            graph.addUndirectedEdge(graph.listOfNodes.get(randomNode1), graph.listOfNodes.get(randomNode2));
+            counter--;
+        }
+
+        return graph;
+    }
+
+    
+
+
+
+
     public static void main(String[] args) {
-        Graph graph1 = new Graph(); //graph1 is the example graph from question 1, 0 is the source, 9 is the destination
-
-        for(int i = 0; i<10; i++){
-            graph1.addNode(i);
-        }
-
-        //adding undirected edges
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(0), graph1.listOfNodes.get(1));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(2));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(3));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(4));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(3), graph1.listOfNodes.get(4));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(4), graph1.listOfNodes.get(5));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(5), graph1.listOfNodes.get(6));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(6), graph1.listOfNodes.get(7));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(7), graph1.listOfNodes.get(8));
-        graph1.addUndirectedEdge(graph1.listOfNodes.get(8), graph1.listOfNodes.get(9));
-
-
+        int n = 5;
+        Graph graph1 = createRandomUnweightedGraphIter(n);
         for (int i = 0; i<graph1.listOfNodes.size(); i++){
             System.out.println("size of neighbors of Node " + i + " is: " + graph1.listOfNodes.get(i).neighbors.size());
         }
-
         System.out.println("\n\n");
-        System.out.println("Neighbors of node 1 are: ");
-        for (int i = 0; i<graph1.listOfNodes.get(1).neighbors.size(); i++){
-            System.out.println(graph1.listOfNodes.get(1).neighbors.get(i).value);
+        int counter = n;
+        for(int j = 0; j<counter; j++){
+            System.out.println("Neighbors of node " + j + " are: ");
+            for (int i = 0; i<graph1.listOfNodes.get(j).neighbors.size(); i++){
+                System.out.println(graph1.listOfNodes.get(j).neighbors.get(i).value);
+            }
         }
 
-        System.out.println("\n\n");
-        System.out.println("Removing edge between 1 and 2");
-        graph1.removeUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(2));
-        System.out.println("After Removal");
-        System.out.println("Neighbors of node 1 are: ");
-        for (int i = 0; i<graph1.listOfNodes.get(1).neighbors.size(); i++){
-            System.out.println(graph1.listOfNodes.get(1).neighbors.get(i).value);
-        }
-
-        System.out.println("\n\n");
-        System.out.println("Removing edge between 4 and 3");
-        graph1.removeUndirectedEdge(graph1.listOfNodes.get(4), graph1.listOfNodes.get(3));
-        System.out.println("After Removal");
-        System.out.println("Neighbors of node 4 are: ");
-        for (int i = 0; i<graph1.listOfNodes.get(4).neighbors.size(); i++){
-            System.out.println(graph1.listOfNodes.get(4).neighbors.get(i).value);
-        }
-
-        System.out.println("\n\n");
-        System.out.println("Removing edge between 1 and 3");
-        graph1.removeUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(3));
-        System.out.println("After Removal");
-        System.out.println("Neighbors of node 3 are: ");
-        for (int i = 0; i<graph1.listOfNodes.get(3).neighbors.size(); i++){
-            System.out.println(graph1.listOfNodes.get(3).neighbors.get(i).value);
-        }
-
-        System.out.println("\n\n");
-        for (int i = 0; i<graph1.listOfNodes.size(); i++){
-            System.out.println("size of neighbors of Node " + i + " is: " + graph1.listOfNodes.get(i).neighbors.size());
-        }
-
-        
         
     }
 }
---
+++ Test.java
@@ -0,0 +1,68 @@
+class Test{
+
+    public static void main(String[] args) {
+        Graph graph1 = new Graph(); //graph1 is the example graph from question 1, 0 is the source, 9 is the destination
+
+        for(int i = 0; i<10; i++){
+            graph1.addNode(i);
+        }
+
+        //adding undirected edges
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(0), graph1.listOfNodes.get(1));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(2));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(3));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(4));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(3), graph1.listOfNodes.get(4));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(4), graph1.listOfNodes.get(5));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(5), graph1.listOfNodes.get(6));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(6), graph1.listOfNodes.get(7));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(7), graph1.listOfNodes.get(8));
+        graph1.addUndirectedEdge(graph1.listOfNodes.get(8), graph1.listOfNodes.get(9));
+
+
+        for (int i = 0; i<graph1.listOfNodes.size(); i++){
+            System.out.println("size of neighbors of Node " + i + " is: " + graph1.listOfNodes.get(i).neighbors.size());
+        }
+
+        System.out.println("\n\n");
+        System.out.println("Neighbors of node 1 are: ");
+        for (int i = 0; i<graph1.listOfNodes.get(1).neighbors.size(); i++){
+            System.out.println(graph1.listOfNodes.get(1).neighbors.get(i).value);
+        }
+
+        System.out.println("\n\n");
+        System.out.println("Removing edge between 1 and 2");
+        graph1.removeUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(2));
+        System.out.println("After Removal");
+        System.out.println("Neighbors of node 1 are: ");
+        for (int i = 0; i<graph1.listOfNodes.get(1).neighbors.size(); i++){
+            System.out.println(graph1.listOfNodes.get(1).neighbors.get(i).value);
+        }
+
+        System.out.println("\n\n");
+        System.out.println("Removing edge between 4 and 3");
+        graph1.removeUndirectedEdge(graph1.listOfNodes.get(4), graph1.listOfNodes.get(3));
+        System.out.println("After Removal");
+        System.out.println("Neighbors of node 4 are: ");
+        for (int i = 0; i<graph1.listOfNodes.get(4).neighbors.size(); i++){
+            System.out.println(graph1.listOfNodes.get(4).neighbors.get(i).value);
+        }
+
+        System.out.println("\n\n");
+        System.out.println("Removing edge between 1 and 3");
+        graph1.removeUndirectedEdge(graph1.listOfNodes.get(1), graph1.listOfNodes.get(3));
+        System.out.println("After Removal");
+        System.out.println("Neighbors of node 3 are: ");
+        for (int i = 0; i<graph1.listOfNodes.get(3).neighbors.size(); i++){
+            System.out.println(graph1.listOfNodes.get(3).neighbors.get(i).value);
+        }
+
+        System.out.println("\n\n");
+        for (int i = 0; i<graph1.listOfNodes.size(); i++){
+            System.out.println("size of neighbors of Node " + i + " is: " + graph1.listOfNodes.get(i).neighbors.size());
+        }
+
+        
+        
+    }
+}
