c443eb9e2d49817053ca84086ce69ad7bfd9a483
Ananya Singh <as2863@njit.edu>
2020-04-16 18:50:02 -0400

    in theory, this should work

--- UnoDoTreCuatroINodeYouWantMe/Node.java
+++ UnoDoTreCuatroINodeYouWantMe/Node.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 class Node{
 
     int value;
-    boolean visted;
+    boolean visited;
     ArrayList<Node> neighbors;
     HashMap<Node, Integer> weights;
 
     public Node(int value){
         this.value = value;
-        this.visted = false;
+        this.visited = false;
         this.neighbors = new ArrayList<Node>();
         this.weights = new HashMap<Node, Integer>();
     }
--- UnoDoTreCuatroINodeYouWantMe/WeightedGraph.java
+++ UnoDoTreCuatroINodeYouWantMe/WeightedGraph.java
@@ -17,7 +17,10 @@ class WeightedGraph{
     }
 
     void addWeightedEdge(final Node first, final Node second, final int edgeWeight){
-
+        if(ifExists(first) && ifExists(second) && checkNeighbor(first, second)){
+            first.neighbors.add(second);
+            first.weights.put(second, edgeWeight);
+        }
     }
 
     void removeDirectedEdge(final Node first, final Node second){
@@ -27,6 +30,29 @@ class WeightedGraph{
     HashSet<Node> getAllNodes(){
         HashSet<Node> nodes = new HashSet<Node>(listOfNodes);
         return nodes;
+    }
+
+    //helper functions
+    boolean ifExists(Node node){
+        if(listOfNodes.contains(node)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    boolean checkNeighbor(Node first, Node second){
+        if(first.neighbors.contains(second)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    void resetVisited(){
+        for(Node node: listOfNodes){
+            node.visited = false;
+        }
     }
 
 
