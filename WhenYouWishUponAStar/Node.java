fd32a708b1f2cfaad14ebe8b0fda906f1cf07659
Ananya Singh <as2863@njit.edu>
2020-04-21 04:56:51 -0400

    started P6

---
+++ WhenYouWishUponAStar/Node.java
@@ -0,0 +1,21 @@
+package WhenYouWishUponAStar;
+
+import java.util.ArrayList;
+
+class Node {
+
+    int value;
+    boolean visited;
+    ArrayList<Node> neighbors;
+    int x;
+    int y;
+
+    public Node(int value){
+        this.value = value;
+        this.visited = false;
+        this.neighbors = new ArrayList<Node>();
+        this.x = x;
+        this.y = y;
+    }
+
+}
