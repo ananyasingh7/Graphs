f346674d640d183583a41700a220e485fa1b3494
Ananya Singh <as2863@njit.edu>
2020-04-23 04:53:31 -0400

    reformated ThankUVertext

--- ThankUVertext/Main.java
+++ ThankUVertext/Main.java
@@ -27,6 +27,41 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        DirectedGraph graph = createRandomDAGIter(30);        
+        DirectedGraph graphRandom = createRandomDAGIter(30);
+        DirectedGraph graph = new DirectedGraph();
+        TopSort top = new TopSort();
+        for(int i = 0; i<8; i++){
+            graph.addNode(i);
+        }
+
+
+        //Node 0 neighbors
+        graph.addDirectedEdge(graph.listOfNodes.get(0), graph.listOfNodes.get(6));
+        graph.addDirectedEdge(graph.listOfNodes.get(7), graph.listOfNodes.get(0));
+
+        //Node 1 neighbors
+        graph.addDirectedEdge(graph.listOfNodes.get(5), graph.listOfNodes.get(1));
+        graph.addDirectedEdge(graph.listOfNodes.get(7), graph.listOfNodes.get(1));
+        graph.addDirectedEdge(graph.listOfNodes.get(1), graph.listOfNodes.get(6));
+        graph.addDirectedEdge(graph.listOfNodes.get(1), graph.listOfNodes.get(4));
+        graph.addDirectedEdge(graph.listOfNodes.get(1), graph.listOfNodes.get(2));
+
+        //Node 2 neighbors
+
+        //Node 3 neighbors
+        graph.addDirectedEdge(graph.listOfNodes.get(3), graph.listOfNodes.get(0));
+        graph.addDirectedEdge(graph.listOfNodes.get(3), graph.listOfNodes.get(4));
+
+        //Node 4 neighbors
+
+        //Node 5 neighbors
+        
+        //Node 6 neighbors
+
+        top.Kahns(graph);
+        graph.resetVisited();
+        System.out.println();
+        top.mDFS(graph);
+        graph.resetVisited();        
     }
 }
